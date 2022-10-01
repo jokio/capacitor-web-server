e82356b24c0a99e89aa09d3fc0d653d13d53f8e8
Ezeki Zibzibadze <ez@jok.io>
2022-10-01 23:20:58 +0400

    feat: initial implementation

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,35 @@
 export interface JokWebServerPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+  getIpAddresses(): Promise<
+    {
+      type: string;
+      ipAddress: string;
+    }[]
+  >;
+
+  start(props: {
+    /**
+     * @default /public
+     */
+    publicFolderPath?: string;
+
+    /**
+     * @default 0.0.0.0
+     */
+    hostname?: string;
+
+    /**
+     * @default 8080
+     */
+    port?: number;
+  }): Promise<boolean>;
+
+  stop(): Promise<void>;
+
+  onRequest(props: {
+    headers: Record<string, string>;
+    method: string;
+    path: string;
+    query: Record<string, string>;
+    body: string;
+  }): Promise<Object>;
 }
--- src/web.ts
+++ src/web.ts
@@ -3,8 +3,29 @@ import { WebPlugin } from '@capacitor/core';
 import type { JokWebServerPlugin } from './definitions';
 
 export class JokWebServerWeb extends WebPlugin implements JokWebServerPlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
+  getIpAddresses(): Promise<{ type: string; ipAddress: string }[]> {
+    throw new Error('Method not implemented.');
+  }
+
+  start(props: {
+    publicFolderPath?: string | undefined;
+    hostname?: string | undefined;
+    port?: number | undefined;
+  }): Promise<boolean> {
+    throw new Error('Method not implemented.');
+  }
+
+  stop(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  onRequest(props: {
+    headers: Record<string, string>;
+    method: string;
+    path: string;
+    query: Record<string, string>;
+    body: string;
+  }): Promise<Object> {
+    throw new Error('Method not implemented.');
   }
 }

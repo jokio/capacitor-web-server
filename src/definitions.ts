375f134148981ea32e94fc1c11b3532158eb3e59
Ezeki Zibzibadze <ez@jok.io>
2022-10-01 23:24:58 +0400

    chore: update capacitor version

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,35 +1,35 @@
 export interface JokWebServerPlugin {
   getIpAddresses(): Promise<
     {
-      type: string;
-      ipAddress: string;
+      type: string
+      ipAddress: string
     }[]
-  >;
+  >
 
   start(props: {
     /**
      * @default /public
      */
-    publicFolderPath?: string;
+    publicFolderPath?: string
 
     /**
      * @default 0.0.0.0
      */
-    hostname?: string;
+    hostname?: string
 
     /**
      * @default 8080
      */
-    port?: number;
-  }): Promise<boolean>;
+    port?: number
+  }): Promise<boolean>
 
-  stop(): Promise<void>;
+  stop(): Promise<void>
 
   onRequest(props: {
-    headers: Record<string, string>;
-    method: string;
-    path: string;
-    query: Record<string, string>;
-    body: string;
-  }): Promise<Object>;
+    headers: Record<string, string>
+    method: string
+    path: string
+    query: Record<string, string>
+    body: string
+  }): Promise<Object>
 }
